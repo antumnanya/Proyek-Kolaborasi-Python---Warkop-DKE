fda7bf3fb05016f35adfed144a00487a44ec3e7a
Kobar Pontolondo - 250211060041 <antumnanya@gmail.com>
2025-11-26 14:05:22 +0800

    Tambah import dan konfigurasi

--- main.py
+++ main.py
@@ -14,3 +14,37 @@ Fitur:
   (disimpan dalam berkas JSON lokal).
 
 """
+
+import tkinter as tk
+from tkinter import simpledialog, messagebox
+import random
+import time
+import json
+import os
+import threading
+
+# ---------------- Config ----------------
+HIGHSCORE_FILE = 'mines_highscores.json'
+DEFAULTS = {
+    'Easy': (9, 9, 10),
+    'Medium': (16, 16, 40),
+    'Hard': (16, 30, 99)
+}
+
+ICONS = {
+    'bomb': '💣',
+    'flag': '🚩',
+    'hidden': '■',
+    'question': '?'
+}
+
+COLOR_MAP = {
+    1: "#0000FF",   # biru
+    2: "#008000",   # hijau
+    3: "#FF0000",   # merah
+    4: "#000080",   # biru navy
+    5: "#800000",   # maroon
+    6: "#008080",   # teal
+    7: "#000000",   # hitam
+    8: "#808080",   # abu
+}
